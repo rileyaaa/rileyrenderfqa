18386c9f72e9645b58c2b642910182ff75cfc4db
rileyaaa <122988789+rileyaaa@users.noreply.github.com>
2023-02-28 10:54:09 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const username = process.env.WEB_USERNAME || "admin";
-const password = process.env.WEB_PASSWORD || "password";
+const username = process.env.WEB_USERNAME || "riley";
+const password = process.env.WEB_PASSWORD || "1003296312";
 const url = process.env.RENDER_EXTERNAL_URL;
 const port = process.env.PORT || 3000;
 const express = require("express");
